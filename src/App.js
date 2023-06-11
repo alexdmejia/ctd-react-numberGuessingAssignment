f4f80e2d510b7a39be924a5f7d6fa5decca2a77e
Alexis <alexdmejia1@gmail.com>
2023-06-10 20:06:11 -0600

    lesson 2.1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
 import NumberGuessingGame from "./NumberGuessingGame";
 import "./App.css";
+import GuessControl from "./GuessControl";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <NumberGuessingGame />
+        <GuessControl/>
       </div>
     );
   }
